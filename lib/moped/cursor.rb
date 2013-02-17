9564822760f0874eae634f0e3250b6cc1b6c941d
Matthew Schulkind <mschulkind@gmail.com>
2013-02-17 18:19:01 -0500

    Apply batch_size correctly to the options.

--- lib/moped/cursor.rb
+++ lib/moped/cursor.rb
@@ -129,10 +129,9 @@ module Moped
     def load_docs
       consistency = session.consistency
       @options[:flags] |= [:slave_ok] if consistency == :eventual
-      @options[:flags] |= [:no_cursor_timeout] if @options[:no_timeout]
 
       options = @options.clone
-      options[:limit] = @options[:batch_size] if @options[:batch_size]
+      options[:limit] = request_limit
 
       reply, @node = session.context.with_node do |node|
         [ node.query(@database, @collection, @selector, options), node ]
